96940f418d731fdc97bfc0078abcc5cbeef925b8
Pavel Sasim <pavels324@gmail.com>
2024-02-11 14:35:55 +0300

    Add Main

---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Icon } from "../icon/Icon";
+
+export const Logo = () => {
+    return (
+        <a href="">
+            <Icon iconId={'reactIcon'} width="50" height="50" />
+        </a>
+    )
+}
---
+++ src/layout/section/main/Main.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+export const Main = () => {
+    return (
+        <div>
+        </div>
+    )
+}
+
+
+const StyledMain = styled.nav`
+`
+
