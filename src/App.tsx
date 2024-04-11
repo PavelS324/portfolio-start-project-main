8bd6932aa4b36c47edfec766584d7a9304a848af
Pavel Sasim <pavels324@gmail.com>
2024-04-11 15:45:06 +0300

    Add section Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import {Header} from './layout/header/Header';
+import { Home } from './layout/section/home/Home';
 
 
 function App() {
     return (
         <div className="App">
             <Header/>
+            <Home/>
         </div>
     );
 }
---
+++ src/layout/section/home/Home.tsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+import MyPhoto from "../../../assets/images/I.webp";
+import { FlexWrapper } from "../../../components/FlexWrapper";
+
+export const Home = () => {
+    return (
+        <StyledHome>
+            <FlexWrapper align={"center"} justify={"space-around"}>
+                <div>
+                    <span>Hi There</span>
+                    <HomeName>I am Pavel Sasim</HomeName>
+                    <HomeTitle>A Front-End Developer</HomeTitle>
+                </div>
+
+                <StyledPhoto src={MyPhoto} alt="" />
+            </FlexWrapper>
+        </StyledHome>
+    );
+};
+
+const StyledHome = styled.div`
+    min-height: 70vh;
+    background-color: #597a97;
+`
+
+
+const StyledPhoto = styled.img`
+    width: 250px;
+    height: 350px; 
+    object-fit: cover;
+`
+
+const HomeTitle = styled.h1`
+    
+`
+
+const HomeName = styled.h2`
+    
+`
+
+
